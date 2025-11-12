c8433483dd8474103eabc9981ea5252b71a941bf
Anjali Yadav <72992224+Anzaliyadav321@users.noreply.github.com>
2025-11-12 13:57:47 +0545

    port changed in config

--- backend/core/config.py
+++ backend/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     MAIL_USERNAME: str = os.getenv("MAIL_USERNAME", "")
     MAIL_PASSWORD: str = os.getenv("MAIL_PASSWORD", "")
     MAIL_FROM: str = os.getenv("MAIL_FROM", "")
-    MAIL_PORT: int = int(os.getenv("MAIL_PORT", 587))
+    MAIL_PORT: int = int(os.getenv("MAIL_PORT", 465))
     MAIL_SERVER: str = os.getenv("MAIL_SERVER", "smtp.gmail.com")
 
 settings = Settings()
